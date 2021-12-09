d4dffe6d359e4e54c10b49fe31d328873035fc94
amananc <amananc06@gmail.com>
2021-12-09 09:19:33 +0530

    Initial Commit

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define mod 1000000007
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+
+int32_t main()
+{
+    fast;
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        for(int i = 0; i < n; i++)
+        cout<<"Hello World!"<<endl;
+    }
+}
