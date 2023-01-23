7bb863746c5e937c14ac2636e5ee092148162303
Aman Anchal <amananc06@gmail.com>
2023-01-24 00:23:45 +0530

    Commit

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define mod 1000000007
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+
+int32_t main()
+{
+    fast;
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        for(int i = 0; i < n; i++)
+        cout<<"Hello World!"<<endl;
+
+        for(int i = 0; i < n; i++)
+        cout<<"Changes are made"<<endl;
+      
+	      // Changes are made in the main branch
+    }
+}
